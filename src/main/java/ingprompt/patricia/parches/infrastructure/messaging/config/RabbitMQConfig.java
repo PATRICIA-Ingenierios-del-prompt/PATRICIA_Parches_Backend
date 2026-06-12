f0358acb94ae5d6f1656422211f90aa3962d6a0f
JuanLeal05 <lecrujuan@gmail.com>
2026-06-12 16:55:18 -0500

    feature: Fix pom dependency an version. Swagger works

--- src/main/java/ingprompt/patricia/parches/infrastructure/messaging/config/RabbitMQConfig.java
+++ src/main/java/ingprompt/patricia/parches/infrastructure/messaging/config/RabbitMQConfig.java
@@ -6,7 +6,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -54,7 +54,7 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
